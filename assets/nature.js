363afb4e13829cc77f44e4bf8c883c8b804fb856
thelaceyedit <thelaceyedit@gmail.com>
2018-07-21 14:03:19 -0400

    nature js ajax call

--- assets/nature.js
+++ assets/nature.js
@@ -1,16 +1,24 @@
-$.ajax({
-    url:https://www.hikingproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=10&key=200318881-809c3921fefd8ff6787a6926acbd003e,
-    method: "GET"
-}).then(function(response){
+var hikingContainer = document.getElementById('hiking-info');
 
-})
+var btn = document.getElementById('btn');
+
+btn.addEventListener('click', function(){
+    var ourRequest = new XMLHttpRequest();
+    ourRequest.open('GET' , ' url:https://www.hikingproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=10&key=200318881-809c3921fefd8ff6787a6926acbd003e'); 
+    ourRequest.onload = function() {
+      var coolData = JSON.parse(ourRequest.responseText);
+      renderHTML(coolData);
+    };
+    ourRequest.send();
+});
+
+function renderHTML(data) {
+    var htmlString = "";
+
+    for (i = 0; i < data.length; i++) {
+        htmlString += "<p>" + data[i].name + "" + "";
+    }
+    hikingContainer.insertAdjacentElement('beforeend' , 'testing 123');
+}
 
 
-// $(document).ready(function () {
-//     var textBoxes = $('input[type="text"]');
-//     textBoxes.focus(function () {
-//         var helpDiv = $(this).attr('id');
-//         $.get('GetHelpText.aspx', { HelpTextKey: helpDiv }, function (response) {
-//             $('#' + helpDiv + 'HelpDiv').html(response.Text);
-//         }, 'json');
-//     });
