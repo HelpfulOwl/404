b156785dbf1b35b2f633e30a47c4000d60cc64ce
Elyam <gregory.j.may@gmail.com>
2018-07-24 13:07:17 -0400

    7/24/18 made changes to .html and weather.js. Also corrected the jquery from slim to minified.

--- assets/weather.js
+++ assets/weather.js
@@ -13,21 +13,39 @@ function APIcall(){
     .then(function(response){
         console.log(response);
         var summary = response.currently.summary; //summary is sunny, mostly cloudy, etc.
-        var temp = response.currently.temperature;//in farenheit.
+        var temperature = response.currently.temperature;//in farenheit.
         var nsd = response.currently.nearestStormDistance; //distance is in kilometers. Need to convert.
-        var cNSD = nsd*0.62;
-        console.log("This is the summary: "+summary);
-        console.log("This is the temperature: "+temp);
-        console.log("This is the distance of the nearest storm: "+nsd);
-        console.log("This is the converted NSD: "+cNSD);
+        var cNSD = nsd*0.62;//converts kilometers to miles.
+        
+        displayWeather(summary,temperature,cNSD);
     });
 };
 
-//function displayWeather ()
+function displayWeather (sum, temperature, nsd) {
+    $(".card-header").text("WEATHER");
+    var cnsd = nsd.toFixed(1);//changes the number of decimal places to 1.
+    var cTemp = temperature.toFixed(1);
+    var summary = $("#summary");
+    var temp = $("#temp");
+    var near = $("#nsd");
+    var title = $(".card-title");
+    console.log(cnsd);
+
+    title.text("Local Weather");
+
+    summary.text(sum);
+    temp.text(cTemp+" Farenheit");
+    near.text("Storm is " +cnsd+" miles away.");
+   
+    console.log("display weather function has been called.");
+    console.log("this is the summary: "+sum);
+    console.log("this is temperature: "+temperature);
+    console.log("this is the nearest storm distance (miles): "+nsd);
+};
 
 
 APIcall();
-console.log("Your source is sauce!");
+
 
 // K * 9/5 - 459.67; to convert kelving to fahrenheit.
 //https://api.darksky.net/forecast/6d0904d150c48c780a450173fe05427a/[latitude],[longitude]
