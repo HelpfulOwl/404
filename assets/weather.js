dc523de852035f8f10701996e97136fda25fa0f6
Elyam <gregory.j.may@gmail.com>
2018-07-25 17:12:46 -0400

    Weather drop down now provides weather info and dates.

--- assets/weather.js
+++ assets/weather.js
@@ -16,8 +16,27 @@ function APIcall(){
         var temperature = response.currently.temperature;//in farenheit.
         var nsd = response.currently.nearestStormDistance; //distance is in kilometers. Need to convert.
         var cNSD = nsd*0.62;//converts kilometers to miles.
+        var time = response.currently.time; //this give 
+        //console.log("TIME: ", moment.unix(time))//this give the current date.
+        //console.log("DAY: ", 
         
         displayWeather(summary,temperature,cNSD);
+
+        for (var i=0; i<3; i++){ //loop for 3 days of upcoming weather summary and temp.
+            var dSum = response.daily.data[i+1].summary;//daily summary.
+            var dTempH = response.daily.data[i+1].apparentTemperatureHigh;///daily High Temperature.
+            var dTempL = response.daily.data[i+1].apparentTemperatureLow;//daily Low Temperature.
+            var dTime = response.daily.data[i+1].time;
+            var cTime = timeConverter(dTime);
+            var wTime = $("#date"+i);
+            var sum = $("#summary"+i);
+            var temH = $("#tempHi"+i);
+            var temL = $("#tempLo"+i);
+            wTime.text(cTime);
+            sum.text(dSum);
+            temH.text(dTempH);
+            temL.text(dTempL);    
+        };
     });
 };
 
@@ -29,20 +48,13 @@ function displayWeather (sum, temperature, nsd) {
     var temp = $("#temp");
     var near = $("#nsd");
     var title = $(".card-title");
-    console.log(cnsd);
-
+    
     title.text("Local Weather");
 
     summary.text(sum);
     temp.text(cTemp+" Farenheit");
     near.text("Storm is " +cnsd+" miles away.");
-   
-    console.log("display weather function has been called.");
-    console.log("this is the summary: "+sum);
-    console.log("this is temperature: "+temperature);
-    console.log("this is the nearest storm distance (miles): "+nsd);
 };
-
 
 APIcall();
 
@@ -50,3 +62,19 @@ APIcall();
 // K * 9/5 - 459.67; to convert kelving to fahrenheit.
 //https://api.darksky.net/forecast/6d0904d150c48c780a450173fe05427a/[latitude],[longitude]
 //current weather currently.temperature... currently.summary
+
+function timeConverter(UNIX_timestamp){
+    var a = new Date(UNIX_timestamp * 1000);
+    var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+    var year = a.getFullYear();
+    var month = months[a.getMonth()];
+    var date = a.getDate();
+    var hour = a.getHours();
+    var min = a.getMinutes();
+    var sec = a.getSeconds();
+    var cTime = date + ' ' + month;
+    console.log(cTime);
+    return cTime;
+  };
+
+  
